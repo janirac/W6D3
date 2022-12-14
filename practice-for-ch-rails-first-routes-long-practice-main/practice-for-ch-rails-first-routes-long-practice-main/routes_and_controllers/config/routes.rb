09a5bac67d4bfb5bab7d39ce4b31a867c7418819
Lynsiearagon <lynsie.aragon@gmail.com>
2022-12-14 13:37:08 -0500

    P1 - resources get thru post complete

--- practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/routes_and_controllers/config/routes.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/routes_and_controllers/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # resources :users
+
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users', to: 'user#index'
+  get 'users/new', to: 'user#new'
+  get 'users/:id/edit', to: 'user#edit'
+  post 'users', to: 'user#create'
+  
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
