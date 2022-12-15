fb8257d3ab250d0e8e18d83b47d9fc50b03311ac
Janira Crispin <janiracrispin@gmail.com>
2022-12-15 17:08:46 -0500

    completed users_controller

--- practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/routes_and_controllers/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/routes_and_controllers/app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     
     def create
         # render json: params
-        user = User.new(params.require(:user).permit(:name, :email))
+        user = User.new(user_params)
         # user.save!
         
         if user.save
@@ -23,4 +23,25 @@ class UsersController < ApplicationController
         render json: user
     end
 
+    def update
+        user = User.find(params[:id])
+
+        if user.update(user_params)
+            redirect_to user_url(user)
+        else
+            render json: user.errors.full_messages, status: 422
+        end 
+    end 
+
+    def destroy
+        user = User.find(params[:id])
+
+        user.destroy
+        redirect_to users_url
+    end 
+
+    private 
+    def user_params 
+      params.require(:user).permit(:name, :email)
+    end
 end
