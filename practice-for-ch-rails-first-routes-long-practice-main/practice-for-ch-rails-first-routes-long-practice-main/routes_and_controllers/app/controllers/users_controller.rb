3c485a92a701e9a332ca34ee06dc4605b8c0d1f7
Janira Crispin <janiracrispin@gmail.com>
2022-12-15 11:54:01 -0500

    users controller create method completed and tested

--- practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/routes_and_controllers/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/routes_and_controllers/app/controllers/users_controller.rb
@@ -7,13 +7,15 @@ class UsersController < ApplicationController
     end
 
     def create
-        render json: params
+        # render json: params
+        debugger
+        user = User.new(params.require(:user).permit(:name, :email))
+        user.save!
+        render json: user
     end
 
     def show 
         render json: params 
     end
-
-    
 
 end
