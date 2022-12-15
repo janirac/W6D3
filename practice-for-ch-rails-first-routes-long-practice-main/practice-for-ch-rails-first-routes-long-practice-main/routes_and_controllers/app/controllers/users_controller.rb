f54fde55e24bcaa5e5988ab7bcaf1968e1c73e72
Lynsiearagon <lynsie.aragon@gmail.com>
2022-12-15 11:00:25 -0500

    render json added to index

--- practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/routes_and_controllers/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/routes_and_controllers/app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
     def index
-        render plain: "I'm in the index action!" 
+        # render plain: "I'm in the index action!" 
+        users = User.all
+        render json: users 
     end
 
     def create
